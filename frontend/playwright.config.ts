a271f3da0cbe4d4c3e1866bc03e90a51d7dbce08
Peter Repukat <orly.alia5@gmail.com>
2026-01-28 04:06:51 +0100

    Fix playwright on arch.....

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
+import { existsSync } from 'fs';
+
+const chromiumPath = existsSync('/usr/bin/chromium') ? '/usr/bin/chromium' : undefined;
+
 export default defineConfig({
-	webServer: { command: 'npm run build && npm run preview', port: 4173 },
-	testDir: 'e2e'
+    webServer: { command: 'npm run build && npm run preview', port: 4173 },
+    testDir: 'e2e',
+    use: {
+        launchOptions: chromiumPath ? { executablePath: chromiumPath } : {}
+    }
 });
