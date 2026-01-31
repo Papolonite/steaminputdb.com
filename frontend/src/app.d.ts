8aeff3f213e31151dbea1afd08a674220cdccc28
Peter Repukat <orly.alia5@gmail.com>
2026-01-31 18:20:09 +0100

    Unplugin Icons

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,3 +1,6 @@
+import 'unplugin-icons/types/svelte';
+
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
@@ -10,4 +13,5 @@ declare global {
     }
 }
 
-export {};
+export { };
+
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { playwright } from '@vitest/browser-playwright';
+import Icons from 'unplugin-icons/vite';
+import { defineConfig } from 'vitest/config';
+
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [
+        sveltekit(),
+        Icons({
+            compiler: 'svelte',
+            autoInstall: true
+        })
+    ],
     test: {
         expect: { requireAssertions: true },
         projects: [
