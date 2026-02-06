17f6b8fb0056bcfb129a0b71da862e90db8d7113
Peter Repukat <orly.alia5@gmail.com>
2026-02-06 21:44:45 +0100

    Fix non optional limit in main search

--- backend/api/search/structs.go
+++ backend/api/search/structs.go
@@ -14,9 +14,9 @@ type Response struct {
 }
 
 type SearchRequestBody struct {
-	Limit        uint32 `json:"limit" max:"20"`
-	LimitGames   uint32 `json:"limit_games" default:"3" max:"5"`
-	LimitConfigs uint32 `json:"limit_configs" default:"5" max:"15"`
+	Limit        uint32 `json:"limit,omitempty,omitzero" max:"20" default:"10"`
+	LimitGames   uint32 `json:"limit_games,omitempty,omitzero" default:"3" max:"5"`
+	LimitConfigs uint32 `json:"limit_configs,omitempty,omitzero" default:"5" max:"15"`
 
 	SearchTerm string `json:"search_term" required:"true" maxLength:"100"`
 }
