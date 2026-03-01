1842a846e4d5bc9f1705a3661393ef536f54a090
Peter Repukat <orly.alia5@gmail.com>
2026-03-01 04:26:55 +0100

    Fix logout

--- frontend/src/routes/logout/+server.ts
+++ frontend/src/routes/logout/+server.ts
@@ -4,5 +4,15 @@ import { redirect, type RequestHandler } from '@sveltejs/kit';
 export const GET: RequestHandler = async (event) => {
     event.cookies.delete('token', { path: '/' });
 
+    event.cookies.set('token', '', {
+        path: '/',
+        domain: 'steaminputdb.com',
+        httpOnly:  true,
+        secure:  true,
+        sameSite: 'lax',
+        expires: undefined,
+        maxAge: 1
+    });
+
     throw redirect(303, resolve('/'));
 };
